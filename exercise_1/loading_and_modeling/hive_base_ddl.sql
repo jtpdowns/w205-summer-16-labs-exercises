3759e5fd8dbe68efe27ba2aed19ea68b3c3c493e
jtpdowns <jtpdowns@josephs-mbp.fios-router.home>
2016-06-19 19:39:58 -0400

    feat: add first set of loading files

---
+++ exercise_1/loading_and_modeling/hive_base_ddl.sql
@@ -0,0 +1,11 @@
+DROP TABLE tablename;
+CREATE EXTERNAL TABLE tablename (col1name, col1type, ...)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+    'separatorChar' = ',',
+    'quoteChar' = '"',
+    'escapeChar' = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/path/in/hdfs';
+
---
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -0,0 +1,2 @@
+tail -n +2 /path/to/original > /path/to/new_file
+
