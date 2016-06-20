ab8b6ab0b8bd4166e76ed4b1c22ee1c1ecb39f13
root <root@ip-172-31-24-127.ec2.internal>
2016-06-20 03:37:46 +0000

    feat: include question in survey

--- exercise_1/transforming/survey.sql
+++ exercise_1/transforming/survey.sql
@@ -1,6 +1,6 @@
 DROP TABLE surveys;
 
 CREATE TABLE surveys AS
-SELECT provider_id, patient_survey_star_rating
+SELECT provider_id, hcahps_question string, patient_survey_star_rating
 FROM survey_data;
 
