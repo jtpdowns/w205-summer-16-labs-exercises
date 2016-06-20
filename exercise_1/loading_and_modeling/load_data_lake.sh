7eca28adc0345f4741b4527fec4de5e4fe10965a
root <root@ip-172-31-24-127.ec2.internal>
2016-06-20 02:00:04 +0000

    fix: correct loading to add layer to directories

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -3,7 +3,11 @@ tail -n +2 data/Timely\ and\ Effective\ Care\ -\ Hospital.csv > data/procedure.c
 tail -n +2 data/HCAHPS\ -\ Hospital.csv > data/survey.csv
 
 hdfs dfs -mkdir /user/w205/exercise_1
-hdfs dfs -put data/hospital.csv /user/w205/hospital_data
-hdfs dfs -put data/procedure.csv /user/w205/procedure_data
-hdfs dfs -put data/survey.csv /user/w205/survey_data
+hdfs dfs -mkdir /user/w205/exercise_1/hospital_data
+hdfs dfs -mkdir /user/w205/exercise_1/procedure_data
+hdfs dfs -mkdir /user/w205/exercise_1/survey_data
+
+hdfs dfs -put data/hospital.csv /user/w205/exercise_1/hospital_data/h_raw
+hdfs dfs -put data/procedure.csv /user/w205/exercise_1/procedure_data/p_raw
+hdfs dfs -put data/survey.csv /user/w205/exercise_1/survey_data/s_raw
 
