aec79b1cc1754273cc17af7db3499b1d194ced84
jtpdowns <jtpdowns@josephs-mbp.fios-router.home>
2016-06-19 20:05:07 -0400

    feat: finish loading data lake

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -1,2 +1,9 @@
-tail -n +2 /path/to/original > /path/to/new_file
+tail -n +2 data/Hospital\ General\ Information.csv > data/hospital.csv
+tail -n +2 data/Timely\ and\ Effective\ Care\ -\ Hospital.csv > data/procedure.csv
+tail -n +2 data/HCAHPS\ -\ Hospital.csv > data/survey.csv
+
+hdfs dfs -mkdir /user/w205/exercise_1
+hdfs dfs -put data/hospital.csv /user/w205/hospital_data
+hdfs dfs -put data/procedure.csv /user/w205/procedure_data
+hdfs dfs -put data/survey.csv /user/w205/survey_data
 
