32cf2988e84e42818ab83610040592ef0aff312b
w205 <w205@ip-172-31-29-180.ec2.internal>
2016-07-25 01:15:14 +0000

    fix: change owner of Tcount to w205 user

--- exercise_2/EX2Tweetwordcount/src/bolts/wordcount.py
+++ exercise_2/EX2Tweetwordcount/src/bolts/wordcount.py
@@ -19,7 +19,7 @@ class WordCounter(Bolt):
         # Database name: Tcount 
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
-        # conn = psycopg2.connect(database="Tcount", user="postgres", password="", host="localhost", port="5432")
+        # conn = psycopg2.connect(database="Tcount", user="w205", password="", host="localhost", port="5432")
         # try:
         # cur.execute("SELECT word, count from Tweetwordcount WHERE word=%s", (word))
         # currentCount = cur.fetchone()[1]
