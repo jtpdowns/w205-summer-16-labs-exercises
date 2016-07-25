833a524821782eada90d285a9774ac93e49dd2cf
w205 <w205@ip-172-31-29-180.ec2.internal>
2016-07-25 01:13:02 +0000

    feat: put (commented) structure for postgres

--- exercise_2/EX2Tweetwordcount/src/bolts/wordcount.py
+++ exercise_2/EX2Tweetwordcount/src/bolts/wordcount.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
-
+# import psycopg2
 
 
 class WordCounter(Bolt):
@@ -19,7 +19,16 @@ class WordCounter(Bolt):
         # Database name: Tcount 
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
-        
+        # conn = psycopg2.connect(database="Tcount", user="postgres", password="", host="localhost", port="5432")
+        # try:
+        # cur.execute("SELECT word, count from Tweetwordcount WHERE word=%s", (word))
+        # currentCount = cur.fetchone()[1]
+        # cur.execute("UPDATE Tweetwordcount SET count=%s WHERE word=%s", (currentCount+1, word))
+        # conn.commit()
+        # except Exception, e:
+        # cur.execute("INSERT INTO Tweetwordcount (word, count) \
+        #     VALUES (%s, 1)", (word));
+        # conn.commit()
 
         # Increment the local count
         self.counts[word] += 1
